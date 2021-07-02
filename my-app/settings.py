3d687e304b96c62e7318997d81ce4d175eb3be2d
Joumana <joumana.el-dakar@etu.univ-grenoble-alpes.fr>
2021-07-02 12:36:51 +0200

    Test heroku db

--- my-app/settings.py
+++ my-app/settings.py
@@ -98,15 +98,18 @@ WSGI_APPLICATION = 'my-app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -117,6 +120,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 
 AUTH_PASSWORD_VALIDATORS = [
     {
